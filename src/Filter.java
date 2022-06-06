0ced532c6d322e1656b155225d6b49eb656cb918
BloodyRanger <BloodyRanger@inbox.ru>
2022-06-07 02:23:23 +0300

    Second Task - prepare before the sleeping

--- src/Filter.java
+++ src/Filter.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Filter {
+
+    protected int treshold;
+
+    public Filter(int treshold){
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source){
+        Logger logger = Logger.getInstance();
+        List<Integer> result = new ArrayList<>();
+        result = source.stream()
+        .filter(value -> value > treshold)
+        .toList();
+        return result;
+    }
 
 
 }
